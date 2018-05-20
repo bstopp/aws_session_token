691b7c8679befebe3dde4d74ed2df90cf8d4958c
bstopp <bryan.stopp@gmail.com>
2018-05-19 20:16:27 -0500

    Adding license headers.

--- lib/aws_session_token/credentials_file.rb
+++ lib/aws_session_token/credentials_file.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
+#
+# AWS Session Token Gem - Tool to wrap AWS API to create and store
+# Session tokens so that other commands/tools (e.g. Terraform) can function as
+# necessary.
+#
+#
+# Copyright 2018 Bryan Stopp <bryan.stopp@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 module AwsSessionToken
 
   # Helper class for interacting with the Credentials file.
